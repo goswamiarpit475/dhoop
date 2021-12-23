f98441be072e0c3934f393d2dbbfeed151c727f5
arpit <goswami.arpit475@gmail.com>
2021-12-23 12:32:18 +0530

    initial setup dhoop

---
+++ our_team.php
@@ -0,0 +1,215 @@
+<?php
+  include('header.php'); 
+?>
+
+
+
+	<div class="page-heading text-center">
+
+		<div class="container zoomIn animated">
+			
+			<h1 class="page-title">ABOUT US <span class="title-under"></span></h1>
+			<p class="page-description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit Necessitatibus.
+			</p>
+			
+		</div>
+
+	</div>
+
+	<div class="main-container">
+
+		<div class="container">
+			<div class="our-team">
+
+	        <div class="container">
+
+	            <h2 class="title-style-1">Our Team <span class="title-under"></span></h2>
+
+	            <div class="row">
+
+	                <div class="col-md-3 col-sm-6">
+
+	                    <div class="team-member">
+
+	                        <div class="thumnail">
+
+	                            <img src="assets/images/team/member-1.jpg" alt="" class="cause-img">
+	                            
+	                        </div>
+
+
+
+	                        <h4 class="member-name"><a href="#">Arpit Goswami</a></h4>
+
+	                        <div class="member-position">
+	                            CO-FOUNDER
+	                        </div>
+
+	                        <div class="btn-holder">
+
+	                          <a href="#" class="btn"> <i class="fa fa-envelope"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-facebook"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-google"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-twitter"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-linkedin"></i> </a>
+	                          
+	                        </div>
+
+	                        
+
+	                    </div> <!-- /.team-member -->
+	                    
+	                </div>
+
+	                <div class="col-md-3 col-sm-6">
+
+	                    <div class="team-member">
+
+	                        <div class="thumnail">
+
+	                            <img src="assets/images/team/member-3.jpg" alt="" class="cause-img">
+	                            
+	                        </div>
+
+
+
+	                        <h4 class="member-name"><a href="#">Varun</a></h4>
+
+	                        <div class="member-position">
+	                            CO-FOUNDER
+	                        </div>
+
+	                        <div class="btn-holder">
+
+	                          <a href="#" class="btn"> <i class="fa fa-envelope"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-facebook"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-google"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-twitter"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-linkedin"></i> </a>
+	                          
+	                        </div>
+
+	                        
+
+	                    </div> <!-- /.team-member -->
+	                    
+	                </div>
+
+
+	                <div class="col-md-3 col-sm-6">
+
+	                    <div class="team-member">
+
+	                        <div class="thumnail">
+
+	                            <img src="assets/images/team/member-4.jpg" alt="" class="cause-img">
+	                            
+	                        </div>
+
+
+
+	                        <h4 class="member-name"><a href="#">Amit Chandel</a></h4>
+
+	                        <div class="member-position">
+	                            CO-FOUNDER
+	                        </div>
+
+	                        <div class="btn-holder">
+
+	                          <a href="#" class="btn"> <i class="fa fa-envelope"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-facebook"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-google"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-twitter"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-linkedin"></i> </a>
+	                          
+	                        </div>
+
+	                        
+
+	                    </div> <!-- /.team-member -->
+	                    
+	                </div>
+
+
+	                <div class="col-md-3 col-sm-6">
+
+	                    <div class="team-member">
+
+	                        <div class="thumnail">
+
+	                            <img src="assets/images/team/member-2.jpg" alt="" class="cause-img">
+	                            
+	                        </div>
+
+
+
+	                        <h4 class="member-name"><a href="#">XYZ</a></h4>
+
+	                        <div class="member-position">
+	                            CO-FOUNDER
+	                        </div>
+
+	                        <div class="btn-holder">
+
+	                          <a href="#" class="btn"> <i class="fa fa-envelope"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-facebook"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-google"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-twitter"></i> </a>
+	                          <a href="#" class="btn"> <i class="fa fa-linkedin"></i> </a>
+	                          
+	                        </div>
+
+	                        
+
+	                    </div> <!-- /.team-member -->
+	                    
+	                </div>
+
+	            </div> <!-- /.row -->
+
+	        </div>
+
+	    </div>
+
+		</div> <!-- /.about-us -->
+
+
+	    
+		
+
+
+	</div>
+
+
+   <?php
+    include('footer.php'); 
+    ?>
+
+
+
+
+       
+        
+        <!-- jQuery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.1.min.js"><\/script>')</script>
+
+        <!-- Bootsrap javascript file -->
+        <script src="assets/js/bootstrap.min.js"></script>
+
+
+        <!-- Template main javascript -->
+        <script src="assets/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ vision.php
@@ -0,0 +1,150 @@
+<?php
+  include('header.php'); 
+?>
+
+
+
+	<div class="page-heading text-center">
+
+		<div class="container zoomIn animated">
+			
+			<h1 class="page-title">MISSION & VISION <span class="title-under"></span></h1>
+			<p class="page-description">
+				Empowering Rural India
+			</p>
+			
+		</div>
+
+	</div>
+
+	<div class="main-container">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-12 fadeIn animated">
+
+					<p>
+						Building communities across rural India with focus on enhancing physical & mental well-being, education, environment, and happiness for underprivileged people.
+					</p>
+
+				</div>
+
+				<div class="col-md-12 fadeIn animated">
+
+					<div id="cause-carousel" class="carousel slide cause-carousel" data-ride="carousel">
+
+						  <!-- Indicators -->
+						  <ol class="carousel-indicators">
+						    <li data-target="#cause-carousel" data-slide-to="0" class="active"></li>
+						    <li data-target="#cause-carousel" data-slide-to="1"></li>
+						  </ol>
+
+						  <!-- Wrapper for slides -->
+						  <div class="carousel-inner" role="listbox">
+
+						    <div class="item active">
+
+						      <img src="assets/images/causes/slider/cause-slider-1.jpg" alt="">
+						      
+						    </div>
+
+						    <div class="item">
+
+						      <img src="assets/images/causes/slider/cause-slider-2.jpg" alt="">
+						      
+						    </div>
+
+						  </div>
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="row fadeIn animated">
+
+				<div class="col-md-6">
+
+					<h2 class="title-style-2"> Vision <span class="title-under"></span></h2>
+
+					<p>
+						Building communities across rural India with focus on enhancing physical & mental well-being, education, environment, and happiness for underprivileged people.
+					</p>
+
+				</div>
+
+				<div class="col-md-6">
+
+					<h2 class="title-style-2"> Mission <span class="title-under"></span></h2>
+
+					<p>
+						We connect people around the country to improve the lives of rural residents. We enable the less fortunate from India villages to think beyond the basics of food, clothing, and shelter using a mix of modern and ancient ideas related to education, health, and happiness.  
+
+						We will achieve this objective by -
+								1.	Organizing camps and awareness for diet, exercise, yoga/meditation, and 	health check-ups 
+								2. 	Supporting local institutions with career counseling, local libraries, 		and remote mentorship.
+								3.	Creating awareness about the government provided benefits.
+								4.	Promoting constructive local traditions and organizing talent shows.
+								5. 	Community driven tree plantation and commercial farming
+								6.	Community built orphanages and old-age homes.
+
+						The results of our progress will be tracked using a holistic happiness index apart from the individual metric for each drive.
+
+					</p>
+
+				</div>
+
+			</div>
+
+			
+
+		</div>
+
+		
+
+		
+
+
+	</div> <!-- /.main-container  -->
+
+
+   <?php
+    include('footer.php'); 
+   ?>
+
+
+
+       
+        
+        <!-- jQuery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.1.min.js"><\/script>')</script>
+
+        <!-- Bootsrap javascript file -->
+        <script src="assets/js/bootstrap.min.js"></script>
+
+        <!-- PrettyPhoto javascript file -->
+        <script src="assets/js/jquery.prettyPhoto.js"></script>
+
+
+
+        <!-- Google map  -->
+        <script src="http://maps.google.com/maps/api/js?sensor=false&amp;libraries=places" type="text/javascript"></script>
+
+
+        <!-- Template main javascript -->
+        <script src="assets/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
